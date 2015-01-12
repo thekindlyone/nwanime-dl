147d6117b008864ede49598dddc309f0889c3fcb
thekindlyone <dodo.dodder@gmail.com>
2015-01-12 09:53:57 +0530

    removed unwanted

--- nwanime-dl/makehtml.py
+++
@@ -1,6 +0,0 @@
-# # coding: utf-8
-import pickle
-table=pickle.load(open('mirror_table.p'))
-html='''<ol>{}</ol>'''.format('\n'.join(['''<li><a href="{}">{}</a></li>'''.format(v,k) for k,v in table.items()]))
-open('table.html','w').write(html)
-
